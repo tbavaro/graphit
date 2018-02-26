c590fee520354358f23f13fc6db11d926e3a7ff2
Tim Bavaro <tbavaro@gmail.com>
2018-02-25 18:16:00 -0800

    simple drag and drop

--- src/NodeView.tsx
+++ src/NodeView.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as Interact from 'interactjs';
 // import MyNodeDatum from './MyNodeDatum';
 import './NodeView.css';
 
@@ -9,14 +10,57 @@ interface Props {
 }
 
 class NodeView extends React.Component<Props, object> {
+  node: HTMLDivElement;
+  interact: Interact.Interactable;
+  dataX = 0;
+  dataY = 0;
+
+  componentDidMount() {
+    this.setInteractions();
+  }
+
+  componentWillReceiveProps() {
+    this.setInteractions();
+  }
+
   render() {
+
     var style = {
       left: this.props.x,
       top: this.props.y
     };
     return (
-      <div className="NodeView" style={style}>{this.props.label}</div>
+      <div
+        ref={this.ref}
+        className="NodeView"
+        style={style}
+      >
+        <div className="NodeView-content">{this.props.label}</div>
+      </div>
     );
+  }
+
+  private ref = (node: HTMLDivElement) => {
+    this.node = node;
+  }
+
+  private setInteractions() {
+    this.interact = Interact(this.node);
+    this.interact.draggable({
+      onmove: this.onDragMove
+    });
+  }
+
+  private onDragMove = (event: Interact.InteractEvent) => {
+    console.log(this.dataX, this.dataY);
+    var x = this.dataX + event.dx;
+    var y = this.dataY + event.dy;
+    console.log(x, y);
+    event.target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+    this.dataX = x;
+    this.dataY = y;
+    // console.log("move", event.x0);
   }
 }
 
