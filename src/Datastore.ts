22967291ac13f37c3eec8ab62605819056617885
Tim Bavaro <tbavaro@gmail.com>
2018-03-02 21:08:53 -0800

    start defining datastore api

---
+++ src/Datastore.ts
@@ -0,0 +1,25 @@
+// import GraphDocument from './GraphDocument';
+
+type Maybe<T> = T | undefined;
+
+class Datastore {
+  // load(): Maybe<string> {
+  //   return undefined;
+  // }
+
+  // save(document: string) {
+  //   return "hello";
+  // }
+
+  loadString(key: string): Maybe<string> {
+    return undefined;
+  }
+
+  saveString(key: string, value: string) {
+
+  }
+
+  private
+}
+
+export default Datastore;
