658c848ffc6684633204e0b04d6411bf5ef9e8be
Tim Bavaro <tbavaro@gmail.com>
2019-04-17 19:51:07 -0700

    log version on startup

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
+console.log(`GraphIt version ${process.env.REACT_APP_VERSION}`);
 ReactDOM.render(
   <App />,
   document.getElementById("root") as HTMLElement
