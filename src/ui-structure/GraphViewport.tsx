3dd3e63f1209d564dfa98b417837e5d6eb42936f
Tim Bavaro <tbavaro@gmail.com>
2019-04-21 11:32:45 -0700

    zoom on search result

--- src/ui-structure/GraphViewport.tsx
+++ src/ui-structure/GraphViewport.tsx
@@ -260,7 +260,7 @@ export class Component extends React.PureComponent<Props, State> {
       this.viewportRef.setZoomState({
         centerX: node.x || 0,
         centerY: node.y || 0,
-        scale: Math.min(1.5, this.props.zoomState.scale)
+        scale: 1
       });
     }
   }
